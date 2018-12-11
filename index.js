4cf2cfb44c9523c1af6e7755f4a6f60867a00586
Alex <asergy34@gmail.com>
2018-12-11 19:42:00 +0300

    Add route /tabletelebot

--- index.js
+++ index.js
@@ -24,6 +24,7 @@ router.get('/moreNews', require('./routes/news_page').get_more);
 router.get('/calendar', require('./routes/calendar_page').get);
 router.get('/calendartelebot', require('./routes/calendar_page').getMatchs);
 router.get('/table', require('./routes/table_page').get);
+router.get('/tabletelebot', require('./routes/table_page').getJSON);
 router.get('/team', require('./routes/team_page').get);
 router.get('/players', require('./routes/team_page').getPlayers);
 router.get('/history', require('./routes/history_page').get);
--- routes/table_page.js
+++ routes/table_page.js
@@ -1,9 +1,29 @@
 // возвращает страницу "Таблица"
-exports.get = async function(ctx, next) {
+// exports.get = async function(ctx, next) {
+
+//     const fillTablePg = require('../functions/fill_TablePage');
+    
+//     const teamArr = await fillTablePg.fill_table(ctx);   
+
+//     await ctx.render('table', {teams: teamArr});    
+//   }
+
+module.exports = {
+  get: async function(ctx, next) {
 
     const fillTablePg = require('../functions/fill_TablePage');
     
     const teamArr = await fillTablePg.fill_table(ctx);   
 
     await ctx.render('table', {teams: teamArr});    
+  },
+  getJSON: async function(ctx, next) {
+
+    const fillTablePg = require('../functions/fill_TablePage');
+    
+    const teamArr = await fillTablePg.fill_table(ctx);   
+
+    ctx.body = teamArr;
+    ctx.status = 200;    
   }
+}
